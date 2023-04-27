b641231a00acc7673b3f423ddff149b3dc48f230
Nischal <gupta.nischal014@gmail.com>
2023-04-27 12:24:34 +0530

    deploying ephemerid python server

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask
+import swisseph as swe
+import datetime
+import juliandate as jd
+from util import get_planet_position, get_degree_minute_zodiac, zodiacData
+# ==============================================================================
+
+app = Flask(__name__)
+
+# ==============================================================================
+
+
+@app.route('/')
+def get():
+    sun = swe.SUN
+    moon = swe.MOON
+    mercury = swe.MERCURY
+    venus = swe.VENUS
+    mars = swe.MARS
+    jupiter = swe.JUPITER
+    saturn = swe.SATURN
+    sun_pos = get_planet_position(sun)
+    moon_pos = get_planet_position(moon)
+    mercury_pos = get_planet_position(mercury)
+    venus_pos = get_planet_position(venus)
+    mars_pos = get_planet_position(mars)
+    jupiter_pos = get_planet_position(jupiter)
+    saturn_pos = get_planet_position(saturn)
+
+    degree, zodiac, minute = get_degree_minute_zodiac(sun_pos)
+    degree1, zodiac1, minute1 = get_degree_minute_zodiac(moon_pos)
+    degree2, zodiac2, minute2 = get_degree_minute_zodiac(mercury_pos)
+    degree3, zodiac3, minute3 = get_degree_minute_zodiac(venus_pos)
+    degree4, zodiac4, minute4 = get_degree_minute_zodiac(mars_pos)
+    degree5, zodiac5, minute5 = get_degree_minute_zodiac(jupiter_pos)
+    degree6, zodiac6, minute6 = get_degree_minute_zodiac(saturn_pos)
+
+    message = {
+        "☀": f"{degree} degree {round(minute)} minute in {zodiacData[zodiac]}",
+        "🌙": f"{degree1} degree {round(minute1)} minute in {zodiacData[zodiac1]}",
+        "☿️": f"{degree2} degree {round(minute2)} minute in {zodiacData[zodiac2]}",
+        "♀️": f"{degree3} degree {round(minute3)} minute in {zodiacData[zodiac3]}",
+        "♂️": f"{degree4} degree {round(minute4)} minute in {zodiacData[zodiac4]}",
+        "♃": f"{degree5} degree {round(minute5)} minute in {zodiacData[zodiac5]}",
+        "♄": f"{degree6} degree {round(minute6)} minute in {zodiacData[zodiac6]}",
+
+    }
+
+    json = {
+        "status": 200,
+        "data": message
+    }
+    return json
+# ==============================================================================
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ util.py
@@ -0,0 +1,64 @@
+import swisseph as swe
+import datetime
+import juliandate as jd
+
+zodiacData = {
+    1: '♈',
+    2: '♉',
+    3: '♊',
+    4: '♋',
+    5: '♌',
+    6: '♍',
+    7: '♎',
+    8: '♏',
+    9: '♐',
+    10: '♑',
+    11: '♒',
+    12: '♓ ',
+}
+
+# =============================================
+
+def get_planet_position():
+    swe.set_ephe_path("C:/Users/NISCHAL/Desktop/cerridwen/swisseph")
+    td = swe.date_conversion(2023, 4, 26, 11.41)
+    pos = swe.calc(2460060.975416667, 4)
+    print("=============================", pos)
+
+
+def get_degree_minute_zodiac(pos):
+    deg = int(pos)
+
+    degree = deg % 30
+    minute = (pos - deg) * 60
+    zodiac = int(deg / 30) + 1
+    return (degree, zodiac, minute)
+
+
+def get_planet_position(planet):
+    swe.set_ephe_path("C:/Users/NISCHAL/Desktop/ephemeris/swisseph")
+    current_time = datetime.datetime.utcnow()
+    time_ist = datetime.datetime.now()
+    planet_num = planet
+    lat = 26.8467
+    lon = 80.9462
+    alt = 123
+    lkoTime = swe.julday(time_ist.year, time_ist.month, time_ist.day,
+                         time_ist.hour,1)
+    istTime = jd.from_gregorian(time_ist.year, time_ist.month, time_ist.day,
+                                time_ist.hour, time_ist.minute,
+                                time_ist.second)
+    # ================ JULIAN_TIME ====================================
+    jday = swe.utc_to_jd(current_time.year, current_time.month,
+                         current_time.day, current_time.hour,
+                         current_time.minute, current_time.second, 1)
+    house_pos = swe.houses(
+        jday[0],
+        lat,
+        lon,
+    )
+    swe.set_topo(lon, lat, alt)
+    swe.set_sid_mode(swe.SIDM_LAHIRI, 0, 0)
+    planet_pos = swe.calc(jday[0], planet_num, swe.FLG_SIDEREAL)
+    # print(house_pos[0][0])
+    return (planet_pos[0][0])
